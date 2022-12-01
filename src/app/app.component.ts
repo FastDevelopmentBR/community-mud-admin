5a175d64de196f9fbbf7eb1927bd0f30ed104cf5
ppereira <pablopereira27@gmail.com>
2022-12-01 17:50:55 -0300

    Incluindo Angular 13 no projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'commud-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'community-mud-admin';
+}
